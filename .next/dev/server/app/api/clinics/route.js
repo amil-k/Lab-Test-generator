870cac91e129ef8036161ec5f6fd064173855e1b
amil-k <kakkatil.amil@gmail.com>
2026-09-05 15:09:57 +0530

    Remove .next build directory

--- .next/dev/server/app/api/clinics/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/clinics/route.js")
-R.c("server/chunks/node_modules_next_1gk7bfu._.js")
-R.c("server/chunks/[root-of-the-server]__0wap6a-._.js")
-R.c("server/chunks/_next-internal_server_app_api_clinics_route_actions_1crn-h-.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/clinics/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/clinics/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
